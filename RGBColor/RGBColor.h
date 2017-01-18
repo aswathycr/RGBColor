8101cc75ce956010c93a41a688594dc8fc022ff8
Aswathy C R <aswathycr.c@ivedix.com>
2017-01-18 08:57:51 +0530

    PublicHeader.h added fro circle view

--- RGBColor/RGBColor.h
+++ RGBColor/RGBColor.h
@@ -15,5 +15,5 @@ FOUNDATION_EXPORT double RGBColorVersionNumber;
 FOUNDATION_EXPORT const unsigned char RGBColorVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <RGBColor/PublicHeader.h>
-
+#import <RGBColor/CircleView.h>
 
