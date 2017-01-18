3ef7735e275dcc27324e868f8e90423b52723bf6
Aswathy C R <aswathycr.c@ivedix.com>
2017-01-18 08:57:01 +0530

    CircleView added

---
+++ RGBColor/CircleView.h
@@ -0,0 +1,15 @@
+//
+//  CircleView.h
+//  RGBColor
+//
+//  Created by Aswathy C R on 18/01/17.
+//  Copyright © 2017 Cabot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CircleView : UIView
+
+@property (nonatomic) CGFloat percent;
+
+@end
---
+++ RGBColor/CircleView.m
@@ -0,0 +1,71 @@
+//
+//  CircleView.m
+//  RGBColor
+//
+//  Created by Aswathy C R on 18/01/17.
+//  Copyright © 2017 Cabot. All rights reserved.
+//
+
+#import "CircleView.h"
+
+@interface CircleView () {
+    CGFloat startAngle;
+    CGFloat endAngle;
+}
+@end
+
+@implementation CircleView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        self.backgroundColor = [UIColor whiteColor];
+        
+        // Determine our start and stop angles for the arc (in radians)
+        startAngle = M_PI * 1.5;
+        endAngle = startAngle + (M_PI * 2);
+        
+    }
+    return self;
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    // Display our percentage as a string
+    NSString* textContent = [NSString stringWithFormat:@"%f", self.percent];
+    
+    UIBezierPath* bezierPath = [UIBezierPath bezierPath];
+    
+    // Create our arc, with the correct angles
+    [bezierPath addArcWithCenter:CGPointMake(rect.size.width / 2, rect.size.height / 2)
+                          radius:130
+                      startAngle:startAngle
+                        endAngle:(endAngle - startAngle) * (_percent / 100.0) + startAngle
+                       clockwise:YES];
+    
+    // Set the display for the path, and stroke it
+    bezierPath.lineWidth = 20;
+    [[UIColor redColor] setStroke];
+    [bezierPath stroke];
+    
+    // Text Drawing
+    CGRect textRect = CGRectMake((rect.size.width / 2.0) - 71/2.0, (rect.size.height / 2.0) - 45/2.0, 71, 45);
+    [[UIColor blackColor] setFill];
+    
+    NSMutableParagraphStyle *paragraphStyle = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
+    /// Set line break mode
+    paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
+    /// Set text alignment
+    paragraphStyle.alignment = NSTextAlignmentCenter;
+    
+    NSDictionary *dictionary = @{ NSFontAttributeName: [UIFont fontWithName: @"Helvetica-Bold" size: 42.5],
+                                  NSParagraphStyleAttributeName: paragraphStyle,
+                                  NSForegroundColorAttributeName: [UIColor redColor],
+                                  };
+    [textContent drawInRect:textRect withAttributes:dictionary];
+    
+}
+
+@end
